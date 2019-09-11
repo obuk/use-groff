c0b1b40d34a5c02c6d3f70f82352c9e164542027
KUBO, Koichi <k@obuk.org>
2019-09-11 22:14:13 +0900

    to use a different name for the patched gropdf

--- eg/setup-gropdf-ubuntu.sh
+++ eg/setup-gropdf-ubuntu.sh
@@ -4,7 +4,7 @@
 
 set -eu
 
-GROPDF=http://git.savannah.gnu.org/cgit/groff.git/plain/src/devices/gropdf
+GROPDF_CURRENT=http://git.savannah.gnu.org/cgit/groff.git/plain/src/devices/gropdf/gropdf.pl
 
 cd ${WORK-work}
 
@@ -22,14 +22,15 @@ s|[@](\w+)[@]|$c{$1}//$&|eg, print while <>;
 END
 
 PATH_GROPDF=`which gropdf`
-curl -L $GROPDF/gropdf.pl | perl -w $CONFIG_PL $PATH_GROPDF >gropdf
+GROPDF=${GROPDF:-"$(basename $PATH_GROPDF)-$(date +%M%d)"}
+curl -L $GROPDF_CURRENT | perl -w $CONFIG_PL $PATH_GROPDF >gropdf
 rm -f $CONFIG_PL
 patch <gropdf.patch
 
-sudo install -m755 gropdf $PATH_GROPDF.p0
+sudo install -m755 gropdf $(dirname $PATH_GROPDF)/$GROPDF
 
 (grep -v ^postpro /usr/share/groff/current/font/devpdf/DESC
- echo postpro $PATH_GROPDF.p0) >DESC
+ echo postpro $GROPDF) >DESC
 sudo install -m644 DESC /usr/share/groff/current/font/devpdf/
 
 # ubuntu
