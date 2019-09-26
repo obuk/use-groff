ccecef575294938112417f5a80f15bb58ac327f5
KUBO, Koichi <k@obuk.org>
2019-09-26 17:01:17 +0900

    rewrite make-textmap.pl

--- make-textmap.pl
+++ make-textmap.pl
@@ -4,111 +4,21 @@ use strict;
 use warnings;
 use File::Basename;
 use File::Spec::Functions;
-use Getopt::Long;
-use XML::Parser::Lite;
-#use Data::Dumper;
-#$Data::Dumper::Indent = 1;
-#$Data::Dumper::Terse = 1;
+# cpanm https://github.com/obuk/font-ttf.git
+use Font::TTF::Font;
 
 my $prog = basename $0, '.pl';
 my $map = shift;
-unless (@ARGV) {
-  die "usage: $prog textmap cmap ...\n";
-}
+die "usage: $prog textmap ttf ...\n" unless my $ttf = shift;
 
 my $groff_bin_path = join ':', grep defined, $ENV{GROFF_BIN_PATH}, "/usr/local/bin", $ENV{PATH};
 my $groff_font_path = join ':', grep defined, $ENV{GROFF_FONT_PATH}, "/usr/local/share/groff/current/font";
 
 my ($sys_map) = grep -f, map catfile($_, "devps/generate", $map), split ':', $groff_font_path;
-
-my %map;
-my %nmap;
-
-sub textmap {
-  my @field = @_;
-  $nmap{$field[0]} += 0;
-  $map{$field[0], $nmap{$field[0]}} = $field[1];
-  $nmap{$field[0]} += 1;
-}
-
-sub mapped {
-  my ($name, $groff_char) = @_;
-  if ($nmap{$name}) {
-    for (map $map{$name, $_}, 0 .. $nmap{$name} - 1) {
-      return 1 if defined && $_ eq $groff_char;
-    }
-  }
-  return 0;
-}
-
 $map && open(MAP, $map) || open(MAP, $sys_map) ||
     die "$prog: can't open '$map' or '$sys_map': $!\n";
-while (<MAP>) {
-  next if /^#/;
-  chop;
-  my @field = split(' ');
-  next if $#field < 0;
-  if ($#field == 1) {
-    if ($field[1] eq "space") {
-      # The PostScript character "space" is automatically mapped
-      # to the groff character "space"; this is for grops.
-      warn "you are not allowed to map to " .
-	"the groff character 'space'";
-    }
-    elsif ($field[0] eq "space") {
-      warn "you are not allowed to map " .
-	"the PostScript character 'space'";
-    }
-    else {
-      textmap(@field);
-    }
-  }
-}
+print for <MAP>;
 close(MAP);
-
-my $cmap_format;
-
-XML::Parser::Lite->new(Handlers => {
-  Start => sub {
-    my ($self, $tag, %args) = @_;
-    if ($tag =~ /^cmap_format_(\d+)$/) {
-      $cmap_format = $1 + 0;
-    }
-    if ($tag eq "map" && $cmap_format && $cmap_format != 6) {
-      my $name = $args{name};
-      if ($args{code}) {
-	my ($code) = map eval, @args{qw/code/};
-	my $groff_char = sprintf "u%04X", $code;
-	if ($name) {
-	  textmap($name, $groff_char) unless mapped($name, $groff_char);
-	}
-      }
-      if ($args{uv} && $args{uvs}) {
-	my ($uv, $uvs) = map eval, @args{qw/uv uvs/};
-	my $groff_char = sprintf "u%04X_%04X", $uv, $uvs;
-	if ($name) {
-	  textmap($name, $groff_char) unless mapped($name, $groff_char);
-	} else {
-	  # If there is no ps glyph name, try Unicode names with both
-	  # 'uni' and 'u' prefixes. see $unicodepsname in afmtodit.
-	  #my @name = (sprintf("u%04X", $uv), sprintf("uni%04X", $uv));
-	  my @name = (sprintf $uv >= 0x10000? "u%04X" : "uni%04X", $uv);
-	  for my $name (@name) {
-	    #print "$cmap_format $name => $groff_char\n";
-	    textmap($name, $groff_char) unless mapped($name, $groff_char);
-	  }
-	}
-      }
-    }
-  },
-  End => sub {
-    my ($self, $tag, %args) = @_;
-    if ($tag =~ /^cmap_format_\d+$/) {
-      $cmap_format = 0;
-    }
-  },
-})->parse(join '', <>);
-
 
 my ($afmtodit) = grep -r, map catfile($_, 'afmtodit'), split ':', $groff_bin_path;
 
@@ -121,11 +31,17 @@ if (open my $f, "<", $afmtodit) {
 }
 
 delete @unicode_decomposed{map sprintf("%X", $_), 0xF900 .. 0xFAFF, 0x2F800 .. 0x2FA1F};
-for my $name (grep $nmap{$_}, map +("uni$_", "u$_"), keys %unicode_decomposed) {
-  delete $map{$name, $_} for 0 .. $nmap{$name} - 1;
-  delete $nmap{$name};
-}
 
-for my $name (sort keys %nmap) {
-  print "$name $map{$name, $_}\n" for 0 .. $nmap{$name} - 1;
+my $f = Font::TTF::Font->open($ttf) || die "$prog: can't open '$ttf': $!\n";
+$f->{cmap}->read;
+$f->{post}->read;
+
+print "# $ttf cmap\n";
+for (grep !$unicode_decomposed{sprintf "%04X", $_}, keys %{$f->{cmap}->find_ms->{val}}) {
+    next unless my $gid = $f->{cmap}->ms_lookup($_);
+    print $f->{post}{VAL}[$gid], ' ', sprintf("u%04X", $_), "\n";
+}
+for (keys %{$f->{cmap}->find_uvs->{val}}) {
+    next unless my $gid = $f->{cmap}->uvs_lookup($_);
+    print $f->{post}{VAL}[$gid], ' ', sprintf("u%04X_%04X", split $;), "\n";
 }
