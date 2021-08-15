933bea6974722c9417c11dbf6b59a9338816fff8
KUBO, Koichi <k@obuk.org>
2021-08-16 08:56:46 +0900

    update FONTPATH

--- files/perldoc.sh
+++ files/perldoc.sh
@@ -16,13 +16,14 @@ search_path() {
 do_perldoc() {
     VERBOSE="${VERBOSE:-}"
     PERLDOC="${PERLDOC:-} -o nroff4"
-    GROFF="${GROFF:-groff -man}"
+    GROFF="${GROFF:-groff -mandoc}"
     TYPESETTER="${TYPESETTER:-}"
     GS="${GS:-gs}"
-    GS_BATCH="${GS_BATCH:-$GS -dNOPAUSE -dQUIET -dBATCH}"
-    GS_PDFWRITE="${GS_PDFWRITE:-$GS_BATCH -sDEVICE=pdfwrite -dPrinted=false}"
+    #GS_BATCH="${GS_BATCH:-$GS -dNOPAUSE -dBATCH -dQUIET}"
+    GS_BATCH="${GS_BATCH:-$GS -dNOPAUSE -dBATCH}"
+    GS_PDFWRITE="${GS_PDFWRITE:-$GS_BATCH -sDEVICE=pdfwrite}"
     # if gs could not find t42 fonts, specify that path 
-    FONTPATH="${FONTPATH:-/usr/local/share/groff/site-font/devps}"
+    FONTPATH="${FONTPATH:-/usr/local/share/groff/site-font/devps:/usr/share/groff/site-font/devps}"
     GS_PDFWRITE="$GS_PDFWRITE -sFONTPATH=$FONTPATH"
 
     while getopts "hVriDtumUFXlTn:d:o:M:w:L:f:q:v:a:-:" opt; do
@@ -49,6 +50,7 @@ do_perldoc() {
                 [ "$lang" = "ja" ] && PERLDOC_BIN=$(which perldocjp | xargs basename)
                 [ -z "$PERLDOC_BIN" ] && PERLDOC="$PERLDOC -L $lang"
                 PERLDOC="$PERLDOC -w __lang=$lang -w __search_path='$(search_path)'"
+                #GROFF="$GROFF -m$lang"
                 ;;
             *)
                 PERLDOC="$PERLDOC -$opt"
@@ -60,6 +62,7 @@ do_perldoc() {
     shift $(($OPTIND - 1))
     
     GROFF="$GROFF -Dutf8 -dlocale=${lang:-C}.UTF-8"
+    #GROFF="$GROFF -k"
     PIPELINE="${PERLDOC_BIN:-perldoc} $PERLDOC $*"
     case "$TYPESETTER" in
         ps)
