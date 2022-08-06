3d95f368a16223bcd15d994bb744e47f925c9a8c
KUBO, Koichi <k@obuk.org>
2022-08-06 20:02:00 +0900

    to change groff repo

--- groff.mk
+++ groff.mk
@@ -8,34 +8,33 @@ VPATH=  ${UG}/files/${OS} ${UG}/files
 
 ifeq ("$(OS)", "ubuntu")
 
-setup::	automake.pkg autoconf.pkg libtool.pkg texinfo.pkg bison.pkg pkgconf.pkg \
-		libuchardet-dev.pkg libxaw7-dev.pkg
-	[ -d groff ] || git clone https://git.savannah.gnu.org/git/groff.git
-	cd groff && git reset --hard
-	cd groff && git pull;
-	cd groff && ./bootstrap && ./configure --prefix=${GROFF_PREFIX}
+GROFF_GIT?=	https://git.savannah.gnu.org/git/groff.git
+#GROFF_GIT?=	https://github.com/t-tk/groff-CJK-font.git
+GROFF_DIR?=	$(shell basename $(GROFF_GIT) .git)
+
+# https://ja.wikipedia.org/w/index.php?search=CJK+Compatibility+Ideographs
+patch_cjk_compat=	perl -i.bak -lne \
+	'next if /^\s*(\{\s*)?"F[9A][0-9A-F]{2}|2F[89A][0-9A-F]{2}",/; print'
+
+all:: automake.pkg autoconf.pkg libtool.pkg texinfo.pkg bison.pkg	\
+		pkgconf.pkg libuchardet-dev.pkg libxaw7-dev.pkg
+	[ -d $(GROFF_DIR) ] || git clone $(GROFF_GIT) $(GROFF_DIR)
+	cd $(GROFF_DIR); \
+	git reset --hard; \
+	git pull; \
+	[ -f ./bootstrap ] && ./bootstrap; \
+	./configure --prefix=${GROFF_PREFIX}
+	cd $(GROFF_DIR); \
+	$(patch_cjk_compat) \
+		src/utils/afmtodit/afmtodit.tables \
+		src/libs/libgroff/uniuni.cpp
+	$(MAKE) -C $(GROFF_DIR) $@
 
 clean::
-	if [ -d groff ]; then \
-		$(MAKE) -C groff $@; \
-	fi
+	-[ -d $(GROFF_DIR) ] && $(MAKE) -C $(GROFF_DIR) $@
 
-all::	setup patch
-	$(MAKE) -C groff $@
-
-patch:	groff.diff
-	[ -d groff ]
-	cd groff && git reset --hard
-	cd groff && patch -p1 <$(abspath $<)
-
-groff.diff:	patch-afmtodit.tables patch-uniuni.cpp
-	cat $^ >$@
-
-clean::
-	rm -f groff.diff
-
-install::
-	sudo make -C groff $@
+install:: all
+	sudo make -C $(GROFF_DIR) $@
 	if [ ! -L "${SITE_TMAC}" -o "$$(readlink ${SITE_TMAC})" != "/etc/groff" ]; then \
 		sudo mv ${SITE_TMAC} ${SITE_TMAC}.old; \
 		sudo ln -s /etc/groff ${SITE_TMAC}; \
